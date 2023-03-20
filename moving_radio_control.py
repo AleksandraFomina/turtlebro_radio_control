94c270e67e481d6033b05944a3fe25817ebdff9f
AleksandraFomina <sashavera1998@gmail.com>
2023-03-20 13:43:51 +0300

    modified:   moving_radio_control.py 	deleted:    reading_from_keyboard.py 	modified:   send_pressed_key.py

--- moving_radio_control.py
+++ moving_radio_control.py
@@ -1,63 +1,28 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import threading
+
+import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
-import serial
+
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
+
+import sys
+from select import select
+
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+import serial
 
 
-class Mover():
-    def __init__(self):
-        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        self.speed = 0.5
-        self.msg = Twist()
-
-    def move(self, mess):
-        rospy.loginfo("Received message: "+mess+" speed:  "+str(self.speed))
-        for c in mess:
-            self.switch(c)
-            self.pub.publish(self.msg)
-        rospy.sleep(1)
-
-    def switch(self, c):
-        if c == " ":
-            pass
-        elif c == "x":
-            self.msg.linear.x = 0
-            self.msg.linear.y = 0
-            self.msg.angular.z = 0
-            pass
-        elif c == "w":
-            self.msg.linear.x = 0.2 * self.speed
-            pass
-        elif c == "a":
-            self.msg.angular.z = 1 * self.speed
-            pass
-        elif c == "s":
-            self.msg.linear.x = -0.2 * self.speed
-            pass
-        elif c == "d":
-            self.msg.angular.z =-1 * self.speed
-            pass
-        elif c == "+":
-            rel = min(self.speed+0.2, 1)/self.speed
-            self.speed = min(self.speed+0.2, 1)
-            self.msg.linear.x = self.msg.linear.x*rel
-            self.msg.linear.y = self.msg.linear.y*rel
-            self.msg.angular.z = self.msg.angular.z*rel
-            rospy.loginfo(" speed:  "+str(self.speed))
-            pass
-        elif c == "-":
-            rel = max(self.speed-0.2, 0.01)/self.speed
-            self.speed = max(self.speed-0.2, 0.01)
-            self.msg.linear.x = self.msg.linear.x*rel
-            self.msg.linear.y = self.msg.linear.y*rel
-            self.msg.angular.z = self.msg.angular.z*rel
-            rospy.loginfo(" speed:  "+str(self.speed))
-            pass
-        elif c == "e":
-            self.msg.linear.y = -0.5 * self.speed
-            pass
-        elif c == "q":
-            self.msg.linear.y = 0.5 * self.speed
-            pass
+TwistMsg = Twist
 
 ser = serial.Serial()
 ser.baudrate = 9600
@@ -68,20 +33,202 @@ ser.open()
 
 ser.flushInput()
 
-if __name__ == '__main__':
-    rospy.init_node("mover")
-    m = Mover()
-    while not rospy.is_shutdown():
-        pack = ser.read(1024)
-        mess = pack.decode().strip().lower()
-        if not mess == "":
-            m.move(mess)
-            
 
-    
-    
-    
 
-    
+moveBindings = {
+        'w':(1,0,0,0),
+        #'o':(1,0,0,-1),
+        'a':(0,0,0,1),
+        'd':(0,0,0,-1),
+       # 'u':(1,0,0,1),
+        's':(-1,0,0,0),
+        #'.':(-1,0,0,1),
+        #'m':(-1,0,0,-1),
+        #'O':(1,-1,0,0),
+        #'I':(1,0,0,0),
+        #'J':(0,1,0,0),
+       # 'L':(0,-1,0,0),
+        #'U':(1,1,0,0),
+       # '<':(-1,0,0,0),
+        #'>':(-1,-1,0,0),
+       # 'M':(-1,1,0,0),
+       # 't':(0,0,1,0),
+       # 'b':(0,0,-1,0),
+    }
 
-   
+speedBindings={
+        'q':(1.1,1.1),
+        'z':(.9,.9),
+        'u':(1.1,1),
+        'j':(.9,1),
+        'e':(1,1.1),
+        'c':(1,.9),
+    }
+
+class PublishThread(threading.Thread):
+    def __init__(self, rate):
+        super(PublishThread, self).__init__()
+        self.publisher = rospy.Publisher('cmd_vel', TwistMsg, queue_size = 1)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.th = 0.0
+        self.speed = 0.0
+        self.turn = 0.0
+        self.condition = threading.Condition()
+        self.done = False
+
+        # Set timeout to None if rate is 0 (causes new_message to wait forever
+        # for new data to publish)
+        if rate != 0.0:
+            self.timeout = 1.0 / rate
+        else:
+            self.timeout = None
+
+        self.start()
+
+    def wait_for_subscribers(self):
+        i = 0
+        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+            if i == 4:
+                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+            rospy.sleep(0.5)
+            i += 1
+            i = i % 5
+        if rospy.is_shutdown():
+            raise Exception("Got shutdown request before subscribers connected")
+
+    def update(self, x, y, z, th, speed, turn):
+        self.condition.acquire()
+        self.x = x
+        self.y = y
+        self.z = z
+        self.th = th
+        self.speed = speed
+        self.turn = turn
+        # Notify publish thread that we have a new message.
+        self.condition.notify()
+        self.condition.release()
+
+    def stop(self):
+        self.done = True
+        self.update(0, 0, 0, 0, 0, 0)
+        self.join()
+
+    def run(self):
+        twist_msg = TwistMsg()
+
+        if stamped:
+            twist = twist_msg.twist
+            twist_msg.header.stamp = rospy.Time.now()
+            twist_msg.header.frame_id = twist_frame
+        else:
+            twist = twist_msg
+        while not self.done:
+            if stamped:
+                twist_msg.header.stamp = rospy.Time.now()
+            self.condition.acquire()
+            # Wait for a new message or timeout.
+            self.condition.wait(self.timeout)
+
+            # Copy state into twist message.
+            twist.linear.x = self.x * self.speed
+            twist.linear.y = self.y * self.speed
+            twist.linear.z = self.z * self.speed
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = self.th * self.turn
+
+            self.condition.release()
+
+            # Publish.
+            self.publisher.publish(twist_msg)
+
+        # Publish stop message when thread exits.
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+        self.publisher.publish(twist_msg)
+
+
+def getKey():
+    key = ser.read(1).decode()
+    #termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+    return key
+
+
+def vels(speed, turn):
+    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+
+if __name__=="__main__":
+    
+    rospy.init_node('teleop_twist_keyboard')
+
+    speed = rospy.get_param("~speed", 0.5)
+    turn = rospy.get_param("~turn", 1.0)
+    speed_limit = rospy.get_param("~speed_limit", 1000)
+    turn_limit = rospy.get_param("~turn_limit", 1000)
+    repeat = rospy.get_param("~repeat_rate", 0.0)
+    key_timeout = rospy.get_param("~key_timeout", 0.5)
+    stamped = rospy.get_param("~stamped", False)
+    twist_frame = rospy.get_param("~frame_id", '')
+    if stamped:
+        TwistMsg = TwistStamped
+
+    pub_thread = PublishThread(repeat)
+
+    x = 0
+    y = 0
+    z = 0
+    th = 0
+    status = 0
+
+    try:
+        pub_thread.wait_for_subscribers()
+        pub_thread.update(x, y, z, th, speed, turn)
+
+        #print(msg)
+        #print(vels(speed,turn))
+        while(1):
+            key = getKey()
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            elif key in speedBindings.keys():
+                speed = min(speed_limit, speed * speedBindings[key][0])
+                turn = min(turn_limit, turn * speedBindings[key][1])
+                if speed == speed_limit:
+                    print("Linear speed limit reached!")
+                if turn == turn_limit:
+                    print("Angular speed limit reached!")
+                print(vels(speed,turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+            else:
+                # Skip updating cmd_vel if key timeout and robot already
+                # stopped.
+                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                    continue
+                x = 0
+                y = 0
+                z = 0
+                th = 0
+                #if (key == '\x03'):
+                 #   break
+
+            pub_thread.update(x, y, z, th, speed, turn)
+
+    except Exception as e:
+        ser.write(e.encode())
+
+    finally:
+        pub_thread.stop()
+        
+
