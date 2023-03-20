b9ed39f239add58a6be316b8bfac9ded025e546e
AleksandraFomina <sashavera1998@gmail.com>
2023-03-20 16:02:03 +0300

    modified:   radio_send_keyboard.py

--- radio_send_keyboard.py
+++ radio_send_keyboard.py
@@ -18,15 +18,10 @@ msg = """
 Reading from the keyboard  and Publishing to Twist!
 ---------------------------
 Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+        w    
+   a    s    d
+   
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
 
 t : up (+z)
 b : down (-z)
@@ -34,7 +29,7 @@ b : down (-z)
 anything else : stop
 
 q/z : increase/decrease max speeds by 10%
-w/x u/j : increase/decrease only linear speed by 10%
+u/j : increase/decrease only linear speed by 10%
 e/c : increase/decrease only angular speed by 10%
 
 CTRL-C to quit
