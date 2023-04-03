a0fc173ff99338ce82840377e5062bcc0012934c
AleksandraFomina <sashavera1998@gmail.com>
2023-04-03 12:08:23 +0300

    modified:   recieve_image_from_radio.py 	modified:   send_image_over_radio.py

--- recieve_image_from_radio.py
+++ recieve_image_from_radio.py
@@ -10,31 +10,24 @@ ser.port = "/dev/ttyUSB0"
 ser.dtr = False
 ser.timeout = 0.5
 ser.open()
-sum = 0
-flag =0
+
 ser.flushInput()
-img_encoded = bytearray()
+img_encoded = bytes()
 
-while flag == 0:
-    pack = ser.read(2)
-    #print("pack ",pack)
-    pack_size = int.from_bytes(pack, "big")
-    #print("size ",pack_size)
-    if pack_size>0:
-        pack = ser.read(1024)
-        while 1:
-            sum += len(pack)
-            #print("sum:   ", sum)
-            img_encoded += bytearray(pack)
-            pack = ser.read(1024)
-            if sum>=pack_size:
-                flag = 1
-                break
-            time.sleep(0.5)
-
-#print('len img: ', len(img_encoded))
-#print(img_encoded)
-np_arr = np.frombuffer(img_encoded, np.uint8)
-image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
-cv2.imwrite("photo.jpg", image)
-#print('ok')
+while 1 :
+    img_encoded = ser.read(1024)
+    #print(pack)
+    if not img_encoded.find(b'\xff\xd8') == -1:
+        #img_encoded += pack
+        while img_encoded.find(b'\xff\xd9') == -1:
+            #print("size:   ", len(img_encoded))
+            img_encoded += ser.read(1024)
+            #img_encoded += pack
+            #time.sleep(0.5)
+        image = cv2.imdecode(np.frombuffer(img_encoded, np.uint8), cv2.IMREAD_GRAYSCALE)
+        #cv2.imwrite("photo.jpg", image)
+        cv2.imshow("photo.jpg", image)
+        cv2.waitKey(1000)
+        img_encoded = bytes()
+    #if cv2.waitKey(10) == 27:
+    #    break
--- send_image_over_radio.py
+++ send_image_over_radio.py
@@ -6,59 +6,61 @@ import serial
 import time
 
 
-rospy.init_node("image_sender")
+class ImageSender():
+        def __init__(self):
+                
+                self.sub = rospy.Subscriber("/front_camera/image_raw/compressed", CompressedImage, self.cb_video_capture)
+                self.image_from_ros_camera = []
+                rospy.init_node("image_sender")
+                
 
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = "/dev/ttyUSB0"
-ser.dtr = False
-ser.timeout = 0.5
-ser.open()
+        def cb_video_capture(self, image_msg):
+                 
+                scale_percent = 50
+                
+                np_arr = np.frombuffer(image_msg.data, np.uint8)
+                self.image_from_ros_camera = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+                
+                width = int(self.image_from_ros_camera.shape[1] * scale_percent / 100)
+                height = int(self.image_from_ros_camera.shape[0] * scale_percent / 100)
+                dsize = (width, height)
 
-
-ser.flushInput()
-
-def cb_video_capture(image_msg):
-        global image_from_ros_camera
-        scale_percent = 50
-        
-        np_arr = np.frombuffer(image_msg.data, np.uint8)
-        image_from_ros_camera = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
-        
-        width = int(image_from_ros_camera.shape[1] * scale_percent / 100)
-        height = int(image_from_ros_camera.shape[0] * scale_percent / 100)
-        # dsize
-        dsize = (width, height)
-
-        # resize image
-        image_from_ros_camera = cv2.resize(image_from_ros_camera, dsize)
+                self.image_from_ros_camera = cv2.resize(self.image_from_ros_camera, dsize)
 
 def img_to_arr(img):
-        quality = 20
+        quality = 10
         _, img_encoded = cv2.imencode('.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, quality])
 
         return img_encoded
+        
+def send(img, pack_size, ser):
+        pack_size = pack_size
+        #img_encoded = img 
+        pack = img.tobytes()
+        
+        pack_num  = int((len(pack))/pack_size)+1
+
+        for i in range(0, pack_num):
+                serial_pack = pack[i*pack_size:(i+1)*pack_size]
+                send_bytes = ser.write(serial_pack)
+                rospy.loginfo("Send data pack %i: %s size 0f message:%i", i, send_bytes, len(pack))
+                time.sleep(1)
+
+if __name__ =="__main__":
+        ser = serial.Serial()
+        ser.baudrate = 9600
+        ser.port = "/dev/ttyUSB0"
+        ser.dtr = False
+        ser.timeout = 0.5
+        ser.open()
+        ser.flushInput()
+
+        sender = ImageSender()
+        pack_size = 1024
+        while not rospy.is_shutdown():
+                img = img_to_arr(sender.image_from_ros_camera)
+                send(img, pack_size, ser)
+                #rospy.sleep(0.1)
 
 
-rospy.Subscriber("/front_camera/image_raw/compressed", CompressedImage, cb_video_capture)
-rospy.sleep(1)
 
-
-
-pack_size = 255
-img_encoded = img_to_arr(image_from_ros_camera)
-pack = img_encoded.tobytes()
-#print("img:  ", img_encoded)
-
-#print("pack ", pack)  
-pack_num  = int((len(pack))/pack_size)+1
-#print("num ", pack_num, "len ", len(pack))
-#print("len ", (len(pack)).to_bytes(2, "big"))
-
-ser.write((len(pack)).to_bytes(2, "big"))
-time.sleep(1)
-for i in range(0, pack_num):
-        serial_pack = pack[i*pack_size:(i+1)*pack_size]
-        send_bytes = ser.write(serial_pack)
-        rospy.loginfo("Send data pack %i: %s", i, send_bytes)
-        time.sleep(3)
